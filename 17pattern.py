e8d0433720351b62b3f55f62243965e6d46ef44d
imhs14 <heerashanker0214@gmail.com>
2026-07-30 02:40:21 +0530

    18 pattern done

--- 17pattern.py
+++ 17pattern.py
@@ -22,7 +22,19 @@ for i in range(1,n+1):
 
 class Solution:
     def pattern17(self, n):
+        class Solution:
+    def pattern17(self, n):
         alph = {1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'J',11:'K',12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T',21:'U',22:'V',23:'W',24:'X',25:'Y',26:'Z'}
+
+        for i in range(1,n+1):
+            print(" "*(n-i),end="")
+            for j in range(1,2*i):
+                if j<= i:
+                    print(alph[j],end="")
+                elif j > i:
+                    i -= 1
+                    print(alph[i],end="")
+            print()
 
         for i in range(1,n+1):
             print(" "*(n-i),end="")
---
+++ 18pattern.py
@@ -0,0 +1,18 @@
+"""
+n = 5
+
+E 
+D E 
+C D E 
+B C D E 
+A B C D E
+"""
+alph = {1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'J',11:'K',12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T',21:'U',22:'V',23:'W',24:'X',25:'Y',26:'Z'}
+
+n = 5
+for i in range(1, n+1):
+    c = n - i + 1        # starting index for this row
+    for j in range(1, i+1):
+        print(alph[c], end=" ")
+        c += 1            # climbing up now, not down
+    print()
