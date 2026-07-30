12e00301e809efb34cb15ad7bfba1f1197b52dc7
imhs14 <heerashanker0214@gmail.com>
2026-07-30 14:52:03 +0530

    pattern 19 done

---
+++ 19pattern.py
@@ -0,0 +1,34 @@
+"""
+n = 5
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+"""
+n = 5
+
+for i in range(n,0,-1):
+    for j in range(1,(n*2)+1):
+        if j <= i:
+            print("*",end="")
+        elif (n*2) - j < i:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
+for i in range(1,n+1):
+    for j in range(1,(n*2)+1):
+        if j <= i:
+            print("*",end="")
+        elif (n*2) - j < i:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
