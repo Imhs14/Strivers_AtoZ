c137fc379233f6c9a7a02a42b6c40d04fd472ad8
imhs14 <heerashanker0214@gmail.com>
2026-07-30 18:21:16 +0530

    pattern 21

---
+++ 21pattern.py
@@ -0,0 +1,20 @@
+"""
+n = 5 
+
+*****
+*   *
+*   *
+*   *
+*****
+
+"""
+n = 4
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i == 1 or i == n:
+            print("*",end="")
+        elif j == 1 or j == n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
