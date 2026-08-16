6659b1064541a545e9e1a82cf33391d560ff9bc3
imhs14 <heerashanker0214@gmail.com>
2026-08-16 21:22:24 +0530

    n to 1 print numbers

---
+++ Recursion/3-Print_N_to_1.py
@@ -0,0 +1,13 @@
+def Nto1(n):
+    if n == 0:
+        return 0
+    print(n)
+    Nto1(n-1)
+Nto1(5)
+
+class Solution:
+    def printNumbers(self, n):
+        if n == 0:
+            return
+        print(n)
+        self.printNumbers(n-1)
