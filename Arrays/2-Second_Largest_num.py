d9f30c91944c9c71b6a585c2ff51a390d6abc2d7
imhs14 <heerashanker0214@gmail.com>
2026-08-20 12:25:48 +0530

    2nd largest done in arrays

--- Arrays/2-Second_Largest_num.py
+++ Arrays/2-Second_Largest_num.py
@@ -1,3 +1,21 @@
 class Solution:
     def secondLargestElement(self, nums):
-        
+        maxx = float('-inf')
+        s_m = float('-inf')
+        for i in range(len(nums)):
+            if nums[i] > maxx:
+                maxx = nums[i]
+
+        for j in range(len(nums)):
+            if nums[j] > s_m and nums[j] != maxx:
+                s_m = nums[j]
+
+        if s_m == float('-inf'):
+            return -1
+
+        return s_m 
+
+p1 = Solution()
+print(p1.secondLargestElement([2,3,4,8,5,9]))
+
+print(p1.secondLargestElement([9,9,9,9,9,9]))
