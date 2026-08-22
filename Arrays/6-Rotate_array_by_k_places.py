8d3c0785972d03829f2a8b68de4e7acb447fd068
imhs14 <heerashanker0214@gmail.com>
2026-08-23 01:26:14 +0530

    rotate array by k places done

---
+++ Arrays/6-Rotate_array_by_k_places.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotateArray(self, nums, k: int) -> None:
+        n = len(nums)
+        k = k % n
+        for i in range(k,n):
+            nums[i-k],nums[i] = nums[i],nums[i-k]
+
+        return nums
+
+p1 = Solution()
+
+print(p1.rotateArray([1,2,3,4,5,6],2))
+
+print(p1.rotateArray([3, 4, 1, 5, 3, -5],8))
